9bc95c3fce6a97239be37844b157861e78156f90
TerryLoc <terryloughran@gmail.com>
2024-01-10 18:43:14 +0000

    props

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -20,12 +25,20 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
 }
+function Item(props) {}
+
 function Stats() {
   return (
-    <footer>
-      You have X items on your list, and you already packed X (X%)
+    <footer className="stats">
+      🧳 You have X items on your list, and you already packed X (X%)
     </footer>
   );
 }
