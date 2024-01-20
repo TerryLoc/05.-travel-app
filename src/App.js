29bfd3314335fe3a39e8a3e8876b69a73b4e5839
TerryLoc <terryloughran@gmail.com>
2024-01-20 11:59:55 +0000

    turner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
   { id: 2, description: 'Charger', quantity: 1, packed: false },
 ];
 
@@ -39,7 +39,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
