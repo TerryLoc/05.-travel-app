a34c24a1bb2ba60e6e8a6293e7b6fa290e6760eb
Terence Loughran <terryloughran@gmail.com>
2024-08-12 18:12:32 +0000

    Feat: Update Form function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -19,7 +21,12 @@ function Logo() {
   return <h1>🌴 Far Away 🌴</h1>;
 }
 function Form() {
-  function handleSubmit() {}
+  const [description, setDescription] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
@@ -31,7 +38,12 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
